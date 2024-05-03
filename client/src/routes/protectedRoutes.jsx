35800d52040638202396ac4c05394d7c85701634
Nadezhda Kireyeva <styyypik@gmail.com>
2024-05-03 17:41:26 +0500

    fix: open board without session with name - correct redirect to main

--- client/src/routes/protectedRoutes.jsx
+++ client/src/routes/protectedRoutes.jsx
@@ -3,9 +3,18 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function ProtectedRoutes() {
   const sessionData = sessionStorage.getItem("persist:root");
   const data = JSON.parse(sessionData);
+
+  if (!data || !data.users) {
+    return <Navigate to="/" />;
+  }
+
   const usersString = data.users;
   const usersData = JSON.parse(usersString);
-  const UserName = usersData.userName;
 
-  return UserName ? <Outlet /> : <Navigate to="/" />;
+  const UserName = usersData.userName;
+  if (!UserName) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 }
